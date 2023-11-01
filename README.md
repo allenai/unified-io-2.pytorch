7f25ba3fad1aef65b6753c9945269ca7598e0b30
Jiasen Lu <echosenm@gmail.com>
2023-11-01 06:53:41 +0000

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Unified-IO-2.pyTorch
+# Unified-IO-2.PyTorch
+
+This repo is pytorch implementation of unified-io-2, which can used for inference evaluation and finetuning. 
+
+Install
+
+```
+git clone unified-io-2.pytorch
+cd unified-io-2.pytorch
+```
+
+```
+pip install -r requirements.txt
+```
+
+(Optional) Use Flash Attention 2 (only available in PyTorch 2.2)
+```
+pip uninstall -y torch
+pip install --pre torch --index-url https://download.pytorch.org/whl/nightly/cu121
+```
+
+## Use the model
+
+
+
+
+## Finetune the model
+
 
---
+++ setup.py
@@ -0,0 +1,29 @@
+# Copyright (c) Jiasen Lu.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+
+from setuptools import find_packages, setup
+
+_PATH_ROOT = os.path.dirname(__file__)
+
+with open(os.path.join(_PATH_ROOT, "README.md"), encoding="utf-8") as fo:
+    readme = fo.read()
+
+setup(
+    name="Unified-IO-2-PyTorch",
+    version="0.1.0",
+    description="Open source large language model implementation",
+    author="Lightning AI",
+    url="https://github.com/lightning-AI/lit-gpt",
+    install_requires=[
+        "torch>=2.1.0",
+        "lightning @ git+https://github.com/Lightning-AI/lightning@800b87eb464bda8defa9425bb0b76651c5c5175b",
+    ],
+    packages=find_packages(),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+)
